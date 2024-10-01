1bc4e1b8ee44c9295760ca74602a8f81ce8bb12d
Lena Felica <leonatdg@mail.ru>
2024-10-01 13:39:43 +0300

    валидация формы плюс проба маски для телефона

--- src/app/scripts/form.js
+++ src/app/scripts/form.js
@@ -1,67 +1,170 @@
-let addForm = document.getElementById('add-form');
-let inputName = document.querySelector('.input-name')
-let inputPhone = document.querySelector('.input-phone');
-let inputEmail = document.querySelector('.input-email');
+const form = document.querySelector('#add-form');
+const inputName = document.querySelector('#name')
+const inputPhone = document.querySelector('#phone');
+const inputEmail = document.querySelector('#email');
 
+const regexName = new RegExp(/^[a-zA-Z0-9]+$/);
+const regexPhone = new RegExp(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/gm);
+const regexEmail = new RegExp(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm);
 
-function validateName(name) {
-  let reg = /^[a-zA-Z0-9]+$/;
-  return reg.test(String(name));
+let formData = {
+  name: {
+    value: "",
+    isValid: false,
+  },
+  phone: {
+    value: "",
+    isValid: false,
+  },
+  email: {
+    value: "",
+    isValid: false,
+  }
+};
+
+function handleNameChange(e) {
+  formData.name.value = e.target.value;
+  formData.name.isValid = true;
+  inputName.classList.remove('error')
 }
 
-function validatePhone(phone) {
-  let reg =  /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-  return reg.test(phone);
+inputName.addEventListener('keydown', handleNameChange);
+inputName.addEventListener('paste', handleNameChange);
+inputName.addEventListener('input', handleNameChange);
+
+function handleChangePhone(e) {
+  formData.phone.value = e.target.value;
+  formData.phone.isValid = true;
+  inputPhone.classList.remove('error');
 }
 
-function validateEmail(email) {
-  let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return reg.test(String(email).toLowerCase());
+inputPhone.addEventListener('keydown', handleChangePhone);
+inputPhone.addEventListener('paste', handleChangePhone);
+inputPhone.addEventListener('input', handleChangePhone);
+
+function handleChangeEmail(e) {
+  formData.email.value = e.target.value;
+  formData.email.isValid = true;
+  inputEmail.classList.remove('error');
 }
 
-addForm.addEventListener('submit',(e) => {
+inputEmail.addEventListener('keydown', handleChangeEmail);
+inputEmail.addEventListener('paste', handleChangeEmail);
+inputEmail.addEventListener('input', handleChangeEmail);
+
+function checkNameValidity() {
+  const isValidName = formData.name.value.match(regexName);
+  formData.name.isValid = isValidName;
+  if(!isValidName) {
+    inputName.classList.add('error');
+    console.log('Name is not valid')
+    return;
+  }
+    inputName.classList.remove('error');
+}
+
+function checkPhoneValidity() {
+  const isValidPhone = formData.phone.value.match(regexPhone);
+  formData.phone.isValid = isValidPhone;
+
+  if(!isValidPhone) {
+    inputPhone.classList.add('error');
+    console.log('Phone is not valid')
+    return;
+  }
+    inputPhone.classList.remove('error');
+}
+
+function checkEmailValidity() {
+  const isValidEmail = formData.email.value.match(regexEmail);
+  formData.email.isValid = isValidEmail;
+
+  if(!isValidEmail) {
+    inputEmail.classList.add('error');
+    console.log('Email is not valid')
+    return;
+  }
+    inputEmail.classList.remove('error');
+}
+
+function clearInputs() {
+  formData = {
+    name: {
+      value: "",
+      isValid: false,
+    },
+    phone: {
+      value: "",
+      isValid: false,
+    },
+    email: {
+      value: "",
+      isValid: false,
+    },
+  };
+
+  inputName.value = "";
+  inputPhone.value = "";
+  inputEmail.value = "";
+}
+
+function checkFormValidity() {
+  checkNameValidity();
+  checkPhoneValidity();
+  checkEmailValidity();
+
+const formNativeValidity = form.checkValidity();
+
+const isAllInputsValid = Object.values(formData).filter((value) => !value.isValid).length;
+
+if(formNativeValidity && Boolean(!isAllInputsValid)) {
+  console.log({
+    name: formData.name.value,
+    phone: formData.phone.value,
+    email: formData.email.value,
+  });
+   clearInputs();
+  }
+}
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
+  checkFormValidity();
+});
 
-  let nameVal = inputName.value;
-  let phoneVal = inputPhone.value;
-  let emailVal = inputEmail.value;
+  const maskPhone = document.querySelectorAll("[data-mask]");
+  for (let i = 0; i < maskPhone.length; i++) {
+    maskPhone[i].addEventListener('input', maskInput);
+  }
+  function maskInput() {
+    let input = this;
+    let mask = input.dataset.mask;
+    let value = input.value;
+    let literalPattern = /[0\*]/;
+    let numberPattern = /[0-9]/;
+    let newValue = "";
+    try {
+      let maskLength = mask.length;
+      let valueIndex = 0;
+      let maskIndex = 0;
 
-  function validation(form) {
-    let result = true;
+      for (; maskIndex < maskLength;) {
+        if (maskIndex >= value.length) break;
 
-    const allInputs = form.querySelectorAll('input');
+        if (mask[maskIndex] === "0" && value[valueIndex].match(numberPattern) === null) break;
 
-    for(const input of allInputs) {
-      if(input.value === "") {
-        input.style.border = '2px solid black';
-        console.log('Field not filled!');
-        result = false;
-      } else {
-        input.style.border = 'none';
-        console.log(input.value)
+        while (mask[maskIndex].match(literalPattern) === null) {
+          if (value[valueIndex] === mask[maskIndex]) break;
+          newValue += mask[maskIndex++];
+        }
+        newValue += value[valueIndex++];
+        maskIndex++;
       }
+
+      input.value = newValue;
+    } catch (e) {
+      console.log(e);
     }
-    return result;
   }
 
-  if(validation(addForm) === true) {
-    alert('Success!')
-  }
 
-  if(!validateName(nameVal)) {
-    console.log('Name not valid');
-    return false;
-  }
-
-  if(!validatePhone(phoneVal)) {
-    console.log('Phone not valid!');
-    return false;
-  };
-
-  if(!validateEmail(emailVal)) {
-    console.log('Email not valid!');
-    return false;
-  };
-
-  console.log(`Name: ${nameVal}\nPhone: ${phoneVal}\nEmail: ${emailVal}`)
-})
