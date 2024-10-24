1b33484174cd088615a62377598d41d2cd82ff8f
Lena Felica <leonatdg@mail.ru>
2024-10-24 16:22:21 +0300

    правки структуры mobile-menu и burger-buttons

--- src/app/scripts/burger-menu.js
+++ src/app/scripts/burger-menu.js
@@ -1,2 +1,11 @@
+const openBurgerBtn = document.querySelector('#open-burger-btn');
+const closeBurgerBtn = document.querySelector('#close-burger-btn');
+const mobileMenu = document.querySelector('#mobile-menu');
+const mobileBtns = document.querySelector('.mobile-buttons')
 
+const openMenuMobile = () => {
+  mobileMenu.classList.add('open');
+  // mobileBtns.classList.remove('active')
+}
 
+openBurgerBtn.addEventListener('click', openMenuMobile)
