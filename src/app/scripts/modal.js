d2dd55cd1d9968553d94f21508a89eede0779045
Lena Felica <leonatdg@mail.ru>
2024-11-12 11:27:13 +0300

    modal adaptive - scss, js

--- src/app/scripts/modal.js
+++ src/app/scripts/modal.js
@@ -4,6 +4,12 @@ const modal = document.querySelector('.modal');
 const tabButtons = document.querySelectorAll('.tabs-menu__btn');
 const contentTabs = document.querySelectorAll('.tabcontent');
 
+const teachersSelect = document.querySelector('.select-teachers')
+const openOptionsBtn = document.querySelector('.select-teachers__btn');
+const selectOptions = document.querySelector('.select-teachers__options');
+const openOptionOne = document.querySelectorAll('.select-teachers__option');
+const closeMobileBtn = document.querySelector('.close-mobile-btn');
+
 const openModal = () => {
   modal.classList.add('open');
 };
@@ -27,7 +33,7 @@ const closeModalOnBackdropClick = (event) => {
 };
 
 window.addEventListener('keydown', (event) => {
-  if (event.key !== 'Escape' || !modal.classList.contains('open')) {
+  if (!event.key == 'Escape' || !modal.classList.contains('open')) {
     return;
   }
 
@@ -62,5 +68,63 @@ const onTabBtnClick = (event) => {
 
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener('click', onTabBtnClick);
-})
+});
+
+//* Select mobile
+//* Клик по кнопке - открыть/закрыть
+const openOptions = () => {
+  selectOptions.classList.toggle('active');
+};
+
+openOptionsBtn.addEventListener('click', openOptions);
+
+//* выбрать элемент из списка / закрыть дропдаун
+function selectedOption (event) {
+  event.stopPropagation();
+  openOptionsBtn.innerText = this.innerText;
+  selectOptions.classList.remove('active');
+}
+
+openOptionOne.forEach ((option) => {
+  option.addEventListener('click', selectedOption)
+});
+
+//* клик снаружи дропдауна - закрыть дропдаун:
+const closeOptionsOnBackdropClick = (event) => {
+  if(event.target !== openOptionsBtn) {
+    selectOptions.classList.remove('active');
+  }
+};
+
+modal.addEventListener('click', closeOptionsOnBackdropClick);
+
+//* крестик
+const closeMobile = () => {
+  modal.classList.remove('open');
+};
+
+closeMobileBtn.addEventListener('click', closeMobile);
+
+//* Options
+const setActiveOption = (optionOne, id) => {
+  optionOne.classList.remove('active');
+  const activeOption = document.querySelector(`.select-teachers__option[data-value="${id}"]`);
+  activeOption.classList.add('active');
+}
+
+const onOptionOneClick = (event) => {
+  const dataId = event.target.getAttribute('data-value');
+
+  contentTabs.forEach((tabContent) => {
+    setActiveTabContent(tabContent, dataId);
+  });
+
+  openOptionOne.forEach((optionOne) => {
+    setActiveOption(optionOne, dataId);
+  });
+}
+
+openOptionOne.forEach((optionOne) => {
+  optionOne.addEventListener('click', onOptionOneClick);
+});
 
